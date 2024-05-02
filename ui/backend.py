60b4574e3dc7f2b5afef36e0c1826c83237c78c9
Remi <rkuba@mit.edu>
2024-05-02 17:35:11 -0400

    connect css

--- ui/backend.py
+++ ui/backend.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import csv
+import pandas as pd
 import io
-from overwrite_csvs import overwrite
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/assets')
 
 
 @app.route('/')
@@ -22,21 +22,37 @@ def upload():
     if file1.filename == '' or file2.filename == '':
         return 'Both files are required'
 
-    if file1 and file2:
-        # Read the first CSV file
-        stream1 = io.StringIO(file1.stream.read().decode("UTF8"), newline=None)
-        csv_reader1 = csv.reader(stream1, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        data1 = next(csv_reader1)  # Read just the first row of the first file
+    try:
+        data1 = get_first_row(file1)
+        data2 = get_first_row(file2)
+    except Exception as e:
+        return str(e)
 
-        # Read the second CSV file
-        stream2 = io.StringIO(file2.stream.read().decode("UTF8"), newline=None)
-        csv_reader2 = csv.reader(stream2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        data2 = next(csv_reader2)  # Read just the first row of the second file
+    return render_template('output.html', data1=data1, data2=data2)
 
-        # You can process data1 and data2 here as per your requirement
 
-        return render_template('output.html', data1=data1, data2=data2)
+def get_first_row(file):
+    # stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
+    # csv_reader = csv.reader(stream, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    # first_row = []
+    # for row in csv_reader:
+    #     first_row.extend(row)
+    # if first_row is None:
+    #     raise ValueError("The CSV file is empty")
+    df = pd.read_excel(file)
+    rows = df.iloc[::]
+    df.to_csv("../AusCycling_CP.csv")
+    print(rows)
+    html_content = rows.apply(generate_string, axis=1).tolist()
+    print(html_content)
+    return html_content
+
+
+def generate_string(row):
+    return f"""M1 {row["Time (s)"]}: {row["M1"]}"""
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+    app.run(debug=True, port=8000)
--- ui/overwrite_csvs.py
+++ ui/overwrite_csvs.py
@@ -1,4 +1,16 @@
 import csv
 
-def overwrite(csv_file):
+
+def overwrite_physiology(csv_file):
+    # TODO: use the physiology file and rewrite the auscyclic W_ij stuff
+    return None
+
+
+def run_gurobi():
+    # TODO: Run Gurobi
+    return None
+
+
+def graph():
+    # TODO: Get the graphs from the return of the gurobi
     return None
